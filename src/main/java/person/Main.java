71d42eade23ebf1663515008d28397ce8ce46977
Molnár András Imre <m.andras.imre00@gmail.com>
2020-04-19 22:31:37 +0200

    Java Faker usage for building Person object.

---
+++ src/main/java/person/Main.java
@@ -0,0 +1,60 @@
+package person;
+
+import com.github.javafaker.Faker;
+import legoset.model.LegoSet;
+import lombok.extern.log4j.Log4j2;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.time.ZoneId;
+import java.util.List;
+
+@Log4j2
+public class Main {
+
+    private static Faker faker = new Faker();
+
+    private static Person randomPerson() {
+        Person person = Person.builder()
+                .name(faker.name().fullName())
+                .dob(faker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate())
+                .gender(faker.options().option(Person.Gender.class))
+                .address(Address.builder()
+                        .city(faker.address().city())
+                        .streetAddress(faker.address().streetAddress())
+                        .country(faker.address().country())
+                        .state(faker.address().state())
+                        .zip(faker.address().zipCode())
+                        .build())
+                .email(faker.internet().emailAddress())
+                .profession(faker.company().profession())
+                .build();
+        return person;
+    }
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-example");
+        EntityManager em = emf.createEntityManager();
+
+        int n = 1000;
+
+        if (args[0].length() > 0) {
+            n = Integer.parseInt(args[0]);
+        }
+
+        em.getTransaction().begin();
+        try {
+            for (int i = 0; i < n; i ++) {
+                em.persist(randomPerson());
+            }
+            em.getTransaction().commit();
+            
+            List<Person> pList = em.createQuery("SELECT p FROM Person p ORDER BY p.id", Person.class).getResultList();;
+            pList.forEach(log::info);
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+}
